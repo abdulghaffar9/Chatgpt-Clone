9b80696b547cf2ea85ad95e16288f0e72ec25a09
curious-abdulghaffar <abdulghaffar4324@gmail.com>
2026-02-11 11:33:42 +0500

    functionalities added

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 // console.log("hello world");
 
 
-//Select the required 
-let apiKey = "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";
+//Select the required html elements
+// let apiKey = "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"; //old one
+let apiKey = "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";
 let answer = document.querySelector(".answer");
 let answerPara = document.querySelector(".answer-para");
 let sendBtn = document.querySelector(".sendBtn");
 let input = document.querySelector(".input");
+console.log(input)
 let h1 = document.querySelector(".title");
 
 
 
 
+//add function to send data to the api
 function getAnswer(userMessage){
     fetch("https://openrouter.ai/api/v1/chat/completions", {
         method: "POST",
@@ -42,11 +45,12 @@ function getAnswer(userMessage){
       })
       .catch(err => {
         console.error("API Error:", err);
+        answerPara.innerHTML = `Sorry an api error occured ${err}`;
       });
       
 }
 
-
+//add onclick event
 sendBtn.addEventListener("click", function () {
     const question = input.value.trim();
     if (!question) return;
@@ -55,6 +59,23 @@ sendBtn.addEventListener("click", function () {
     getAnswer(question);
     input.value = "";
   });
+
+
+  //add input search functionality
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault(); // prevents form submission if inside a form
+      sendBtn.click();
+    }
+  });
+
+
+
+
+
+
+
+
   
 
 
